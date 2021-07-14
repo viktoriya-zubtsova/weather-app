08e9dcf6587f19808dc33c0ba997af62d01a42d1
Viktoriya Zubtsova <wow55222@yandex.ru>
2021-07-14 16:01:38 +0300

    на проверку

--- src/App/App.js
+++ src/App/App.js
@@ -1,38 +1,34 @@
 import React from 'react';
-import './App.css';
-import './sun.svg';
-import './rain.svg';
-import './cloudy.svg';
-import './cloud.svg';
-import './storm.svg';
+import classnames from 'classnames';
+import styles from './App.module.css';
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?id=542420&lang=ru&appid=f244a34161f4e475edb307cbc293b72c';
 
 class App extends React.Component {
   state = {
     celsius: true,
-    error: false
+    error: false,
+    textError: 'Информация недоступна.'
   }
 
-  componentDidMount = () => {
+  componentDidMount() {
     fetch(url)
     .then(res => res.json())
     .then(data => {
       this.setState({
         sity: data.name,
         description: data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1).toLowerCase(),
-        wind: data.wind.speed,
+        wind: Math.round(data.wind.speed),
         deg: data.wind.deg,
         wet: data.main.humidity,
         rain: data.clouds.all,
         icon: data.weather[0].id,
-        temp: (Math.round(data.main.temp/10)),
+        temp: data.main.temp,
         press: data.main.pressure
       });
       }).catch(error => {
           this.setState({
-            error: true,
-            textError: 'Данные недоступны.'
+            error: true
           })
         });
   }
@@ -40,6 +36,7 @@ class App extends React.Component {
   render() {
     let direction = this.state.deg;
     let iconImg = this.state.icon;
+    let temp = this.state.temp;
 
     const getWindDirection = () => {
       switch (true) {
@@ -107,63 +104,65 @@ class App extends React.Component {
       }
     }
 
+    const isCelsiusTemp = () => {
+      if (this.state.celsius) {
+        temp = (Math.round(this.state.temp * 0.1));
+      } else {
+        temp = Math.round(this.state.temp * 0.18 + 32);
+      }
+    }
+
     getWindDirection();
     getIconImg();
+    isCelsiusTemp();
 
-    return (
-      <div className="App">
+    if (this.state.error) {
+      return (<div>
+        <h2 className={styles.title}>{this.state.textError}</h2>
+        </div>);
+      } else {
+     return (
+      <div className={styles.wrap}>
         <header>
           <div>
-            <h1 className="title">{this.state.sity}</h1>
-            <span className="sityLink">Сменить город</span>
-            <span className="locationLink">Мое местоположение</span>
+            <h1 className={styles.title}>{this.state.sity}</h1>
+            <span className={styles.sityLink}>Сменить город</span>
+            <span className={styles.locationLink}>Мое местоположение</span>
           </div>
-          <div className="buttonGroup">°
-            <button className="buttonActive">C</button>
-            <button>F</button>
+          <div className={styles.buttonGroup}>°
+            <button
+              className={classnames({ [styles.buttonActive]: this.state.celsius })}
+              onClick={() => {this.setState({ celsius: true })}}>C</button>
+            <button
+              className={classnames({ [styles.buttonActive]: !this.state.celsius })}
+              onClick={() => {this.setState({ celsius: false })}}>F</button>
           </div>
-          </header>
-          <section>
-            <img className="icon" src={iconImg} alt="icon"/>
-            <h1 className="temp">{this.state.temp}°</h1>
-            <p className="description">{this.state.description}</p>
-            <div className="bottom">
-              <div className="bottomItem">Ветер
-                <p>{this.state.wind} м/с{direction}</p>
-              </div>
-              <div className="bottomItem">Давление
-                <p>{this.state.press} мм рт. ст.</p>
-              </div>
-              <div className="bottomItem">Влажность
-                <p>{this.state.wet}%</p>
-              </div>
-              <div className="bottomItem">Вероятность дождя
-                <p>{this.state.rain}%</p>
-              </div>
+        </header>
+        <section>
+          <img className={styles.icon} src={iconImg} alt='icon'/>
+          <h1 className={styles.temp}>{temp}°</h1>
+          <p className={styles.description}>{this.state.description}</p>
+          <div className={styles.bottom}>
+            <div className={styles.bottomItem}>
+              <span>Ветер</span>
+              <p>{this.state.wind} м/с{direction}</p>
             </div>
-          </section>
-        </div>
-      );
-
-      // const button1 = document.querySelectorAll('button')[0];
-      // const button2 = document.querySelectorAll('button')[1];
-      //
-      // document.querySelector('.buttonGroup').onclick = () => {
-      //   console.log(1);
-      //   if (!this.state.celsius) {
-      //     button1.classList.add('buttonActive');
-      //     button2.classList.remove('buttonActive');
-      //     this.setState({
-      //       celsius: true
-      //     });
-      //   } else {
-      //     button2.classList.add('buttonActive');
-      //     button1.classList.remove('buttonActive');
-      //     this.setState({
-      //       celsius: false
-      //     });
-      //   }
-      // }
+            <div className={styles.bottomItem}>
+              <span>Давление</span>
+              <p>{this.state.press} мм рт. ст.</p>
+            </div>
+            <div className={styles.bottomItemSlim}>
+              <span>Влажность</span>
+              <p>{this.state.wet}%</p>
+            </div>
+            <div className={styles.bottomItemSlim}>
+              <span>Вероятность дождя</span>
+              <p>{this.state.rain}%</p>
+            </div>
+          </div>
+        </section>
+      </div>
+      );}
     }
   }
 
